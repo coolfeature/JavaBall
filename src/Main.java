f64f0ce22c30dcd40f7c66d308a8e05c0dab2af6
sczaja <sczaja@KF96SPXZ1.KFIS.LOCAL>
2015-01-18 10:02:20 +0000

    Merging with Patrick's Chart. Introduced color to bars and centred labels.

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@
 import n.db.DataSource;
 import n.gui.Container;
 
-/*This is the main class which instantiates a DataSource object identified as 'filestore'
- * which creates the list of Referee objects, also created within the main constructor is the 
- * Gui which is a Container object
+/**
+ * This is the main method which instantiates a DataSource object and the main 
+ * Gui class
  */
 public class Main {
 	public static void main(String[] args) {
 		//DataSource object is instantiated with the reference name fileStore
-		DataSource fileStore = new DataSource();
+		DataSource dataSource = new DataSource();
 		
-		/*Container object is instantiated with the identifier 'gui', this has been created 
+		/* Container object is instantiated with the identifier 'gui', this has been created 
 		 * within an invoke later method which ensures that any changes to be made to the GUI
 		 * on the EDT to ensure thread safety
 		 */
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Container gui = new Container(fileStore);
+                Container gui = new Container(dataSource);
               //set the gui visibility to true as soon as the program runs
                 gui.setVisible(true);
             }
--- src/n/gui/Chart.java
+++ src/n/gui/Chart.java
@@ -1,8 +1,8 @@
 package n.gui;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -14,76 +14,75 @@ public class Chart extends JPanel {
 
 	public static final String TAB_NAME = "Chart";
 	private static final long serialVersionUID = 1L;
-	DataSource dataSource;
 	
+	/** Gap between each bar in pixels */
+	private static final int GAP = 10;	
+	
+	/** Data source containing referee information */
+	private DataSource dataSource;
+	/** Width of bar in pixels */
+	private int width;
+	/** Vertical position of the bottom of the bar */
+	private int bottom;
+	/** Number of pixels representing one match allocation */
+	private int pixelPerAllo;
+
+
 	/**
-	 * The idea is to display a bar chart showing each referee's allocations.
-	 * @param fileStore
+	 * Constructor setting fileStore
+	 * @param fileStore data store containing list of referees
 	 */
 	public Chart(DataSource fileStore) {
-		this.dataSource = fileStore;    
-		repaint();
+		this.dataSource = fileStore;
 	}
-	
-	public void refresh() {
-		
-	}
-	
-	@Override
-	protected void paintComponent(Graphics g)
-	{	
-		// LinkedHashMap as insertion order is important  
-		Map<String, Referee> chartBars = new LinkedHashMap<String, Referee>();
-		Referee[] referees = dataSource.getReferees();
-		
-		for (Referee r : referees) {
-			chartBars.put(r.getId(), r);
-		}
-		
-		// get max allocations
-		int max = Integer.MIN_VALUE;
-		for (Referee r : chartBars.values()) {
-			max = Math.max(max, r.getAllocations());
-		}
-		
-		// left padding
-		int widthPad = 30;
-		
-		// top padding
-		int heightPad = 30;
-		
-		// width of one column
-		int width = ((getWidth() - (2*widthPad)) / chartBars.size()) - 2;
 
-		// draw y line
-		g.drawLine(widthPad, heightPad, widthPad, (getHeight() - (2*heightPad)));
-		
-		
-/*		int divisionLineXStart = (widthPad - 3);
-		int divisionLineXEnd = (widthPad + 3);
-		for (int i=1;i<max+1;i++) {
-			int divisionLineY = (int) ((getHeight()-(2*heightPad)) * ((double)i / max));
-			g.drawLine(divisionLineXStart, divisionLineY, divisionLineXEnd, divisionLineY);
+
+	/**
+	 * Method to paint a single bar
+	 * @param g The graphics object to draw to
+	 * @param x Position on the x-axis
+	 * @param referee object being displayed as bar
+	 */
+	private void paintBar(Graphics2D g, int x, Referee referee){
+		int barTop = bottom-referee.getAllocations()*pixelPerAllo;
+		g.setColor(Color.green);
+		g.fillRect(x, barTop, width, referee.getAllocations()*pixelPerAllo);
+		g.setColor(Color.black);
+		g.drawString(referee.getId(), x + (width/3), bottom+20);
+		g.drawString("" + referee.getAllocations(), x + (width/3), barTop - 12);
+	}
+
+	/** Method to set the number of pixels inside the bar representing 1 allocation */
+	private void setAllocationPixel (){
+		int max = 0;
+		for(Referee ref : dataSource.getReferees()) {
+			if (ref.getAllocations() > max){
+				max = ref.getAllocations();
+			}
 		}
-		g.drawLine(widthPad, heightPad, widthPad, (getHeight() - (2*heightPad)));*/
-		
-		for (String id : chartBars.keySet())
+		pixelPerAllo = (this.getHeight() - 60) / max;
+	}
+
+	/**
+	 * Updates the display by painting the bar chart
+	 * @param g the Graphics object
+	 */  
+	public void paintComponent(Graphics g)
+	{  
+		// Recover Graphics2D
+		Graphics2D g2 = (Graphics2D) g;
+		Referee[] refs = dataSource.getReferees();
+		width = (this.getWidth()-(refs.length+1)*GAP) / refs.length; 
+		setAllocationPixel();
+		bottom = this.getHeight()-30;
+
+		//sets font for label
+		g2.setFont(new Font("Monospaced", Font.BOLD, 18));
+
+		//iterated over referee array and paints the corresponding bar
+		for (int i = 0; i < refs.length; i++)
 		{
-			Referee ref = chartBars.get(id);
-			// the height of the bar 
-			int height = (int) ((getHeight()-(2*heightPad)) * ((double)ref.getAllocations() / max));
-			g.setColor(Color.green);
-			// fill column
-			g.fillRect(widthPad, getHeight() - height - heightPad, width, height);
-			g.setColor(Color.black);
-			// draw column
-			g.drawRect(widthPad, getHeight() - height - heightPad, width, height);
-			// draw number of allocations above each bar
-			g.drawString(Integer.toString(ref.getAllocations()), widthPad  + (width/3), getHeight() - height - heightPad);
-			// show id labels for columns
-			g.drawString(id, widthPad + (width/3), getHeight() - (heightPad/2));
-			// set the x padding for the next column
-			widthPad += (width + 2);
+			paintBar(g2, (i+1)*GAP+i*width, refs[i]);
 		}
 	}
 }
