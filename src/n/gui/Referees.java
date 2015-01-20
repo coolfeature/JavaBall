1ad7b8c30c76d475939390fd302f340b8329937c
danny-g <danny-g@users.noreply.github.com>
2015-01-20 10:14:25 +0000

    Comments added

--- src/n/gui/Referees.java
+++ src/n/gui/Referees.java
@@ -16,8 +16,12 @@ import n.models.Referee;
  */
 public class Referees extends JPanel {
 	
+	/*declares the instance variables for this class as final (as they will not change), 
+	 * which is the serialVersionUID; and the tab name that is displayed at the top of the Gui */
 	private static final long serialVersionUID = 1L;
 	public static final String TAB_NAME = "Referees";
+	
+	//an instance of DataSource and JTable (to display list of referees) will also be used within this class
 	DataSource dataSource = null;
 	JTable table = null;
 	
@@ -27,9 +31,17 @@ public class Referees extends JPanel {
 	 */
 	public Referees(DataSource dataStore) {
 		this.dataSource = dataStore;
+		
+		//set the layout for the Referee tab to BorderLayout
 		this.setLayout(new BorderLayout());
+		
+		//instantiate and initialise the table variable as a JTable
 		table = new JTable();
+		
+		//call on the refreshTableData method within this class to populate the table with Referee object information
 		refreshTableData();
+		
+		//instantiate a JScrollPane and add this to the table within the Referees tab on the gui
 		JScrollPane scrollPane = new JScrollPane(table);
 		this.add(scrollPane, BorderLayout.NORTH);
 		this.setPreferredSize(new Dimension(300, 300));
